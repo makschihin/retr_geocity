b8d5a54c2f1ae9e5f6182f12c502995bb4567a56
makschihin <maksymchihin@gmail.com>
2022-05-30 19:14:01 +0300

    Update

--- target/generated-sources/annotations/com/softserveinc/geocitizen/dto/mapper/RegisteredUserMapperImpl.java
+++ target/generated-sources/annotations/com/softserveinc/geocitizen/dto/mapper/RegisteredUserMapperImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2022-05-26T18:32:09+0300",
+    date = "2022-05-27T12:34:04+0300",
     comments = "version: 1.2.0.Final, compiler: Eclipse JDT (IDE) 1.4.100.v20220318-0906, environment: Java 17.0.2 (Eclipse Adoptium)"
 )
 @Component
